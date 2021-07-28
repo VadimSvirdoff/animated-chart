a0613e41de7eb18a106b4806bb793a2c02dd2586
Vadym Svyrydov <avangardsv@gmail.com>
2021-07-28 23:21:53 +0300

    add typescipt to point

--- src/components/point/Point.tsx
+++ src/components/point/Point.tsx
@@ -2,38 +2,47 @@
 import { convertToPercents } from 'src/services';
 import './point.css';
 
-const Point = ({
+
+type IPoint = (arg: {
+    x: number,
+    y: number,
+    value: string,
+    isFinalScreenStage: boolean,
+    pointIcon: string,
+    multiplier: number
+
+}) => JSX.Element | null;
+
+const Point: IPoint = ({
     x,
     y,
     value,
     isFinalScreenStage,
     pointIcon,
     multiplier
-}) => multiplier
+}) => {
+    return multiplier
         ? (
             isFinalScreenStage
                 ? (
                     <text x={convertToPercents(x)} y={convertToPercents(y)} className='point-final-text'>{value}</text>
                 )
                 : (
-                    <>
                         <g className='point'>
 
                             <image
                                 x={x + 10}
                                 y={y + 42}
                                 href={pointIcon}
-                                alt="🚀"
                                 className="point-label"
                             />
                             <text x={x + 20} y={y + 15} className='point-text'>{value}</text>
 
                         </g>
-
-                    </>
                 )
         )
         : null
+    }
 
 
 export default Point
